79c0fca4890759783e6df7b3e3a36d6fca67c100
RSamaium <sam@newbot.io>
2023-06-05 11:55:51 +0200

    users module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core'
     selector: 'app-root',
     template: `
         <app-navbar />
+        <app-users />
     `
 })
 export class AppComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core'
 import { AppComponent } from './app.component'
 import { BrowserModule } from '@angular/platform-browser'
 import { NavbarModule } from './navbar/navbar.module'
+import { UsersModule } from './users/users.module'
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, NavbarModule],
+    imports: [BrowserModule, NavbarModule, UsersModule],
     bootstrap: [AppComponent]
 })
 export class AppModule {}
