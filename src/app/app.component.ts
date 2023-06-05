57005a3aca8e37f35677e0fafecb15f9dd5e159a
RSamaium <sam@newbot.io>
2023-06-05 10:58:21 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,7 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    template: '<h1>test</h1>'
 })
-export class AppComponent {
-  title = 'myapp';
-}
+export class AppComponent {}
