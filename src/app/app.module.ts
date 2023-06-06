5f0d5746b1e2f3e328b0499f813fe9283fcf49c3
RSamaium <sam@newbot.io>
2023-06-06 12:14:09 +0200

    user service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { LoginModule } from './login/login.module'
 import { appRouter } from './app.router'
 import { LayoutModule } from './layout/layout.module'
 import { AppService } from './core/services/app.service'
+import { HttpClientModule } from '@angular/common/http'
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, LoginModule, LayoutModule, appRouter],
+    imports: [BrowserModule, LoginModule, LayoutModule, appRouter, HttpClientModule],
     bootstrap: [AppComponent],
     providers: [AppService]
 })
