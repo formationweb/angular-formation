3166602588776cc74c95b052706ee0518be04fcd
RSamaium <sam@newbot.io>
2023-06-07 15:45:46 +0200

    user edit - part 2

--- src/app/layout/user-edit/user-edit.component.ts
+++ src/app/layout/user-edit/user-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Subscription, switchMap } from 'rxjs';
 import { User } from 'src/app/core/interfaces/user';
@@ -12,10 +13,17 @@ import { UserService } from 'src/app/core/services/user.service';
 export class UserEditComponent implements OnInit, OnDestroy {
   user: User = {} as User
   subscription!: Subscription
+  propName: FormControl = new FormControl()
+  form: FormGroup = this.builder.group({
+    name: this.propName,
+    username: '',
+    email: ''
+  })
   
   constructor(
     private route: ActivatedRoute,
-    private userService: UserService
+    private userService: UserService,
+    private builder: FormBuilder
   ) { }
 
   ngOnInit(): void {
@@ -34,7 +42,13 @@ export class UserEditComponent implements OnInit, OnDestroy {
       )
       .subscribe((user: User) => {
         this.user = user
+       // this.propName.setValue(this.user.name)
+       this.form.patchValue(this.user)
       })
+  }
+
+  edit() {
+
   }
 
   ngOnDestroy(): void {
--- src/app/layout/user-edit/user-edit.module.ts
+++ src/app/layout/user-edit/user-edit.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserEditComponent } from './user-edit.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
@@ -9,7 +10,8 @@ import { UserEditComponent } from './user-edit.component';
     UserEditComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule
   ]
 })
 export class UserEditModule { }
