ab88cb360dac166db509284d1711c5250b198e59
RSamaium <sam@newbot.io>
2023-06-07 12:05:33 +0200

    validator

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,28 +1,37 @@
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { Form, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { domainValidator } from '../shared/validators/domain.validator';
 
 @Component({
   selector: 'app-login',
-  templateUrl: './login.component.html',
-  //styleUrls: ['./login.component.css']
-  styles: [
-    `
-      .red {
-        color: red;
-      }
-      .green {
-        color: green;
-      }
-    `
-  ]
+  templateUrl: './login.component.html'
 })
-export class LoginComponent {
-  //@ViewChild('myform') form!: NgForm
+export class LoginComponent implements OnInit {
+  propEmail: FormControl = new FormControl('', [
+    Validators.required,
+    Validators.minLength(3),
+    domainValidator('hotmail.com')
+  ])
+  propPass: FormControl = new FormControl()
+  myform: FormGroup = this.builder.group({
+    email: this.propEmail,
+    password: this.propPass
+  })
+  submitted: boolean = false
 
-  login(form: NgForm) {
-    if (form.invalid) {
+  constructor(private builder: FormBuilder) { }
+
+  ngOnInit(): void {
+    /*setTimeout(() => {
+     this.propEmail.setValue('toto')
+    }, 1000)*/
+  }
+
+  login() {
+    this.submitted = true
+    if (this.myform.invalid) {
       return
     }
-    console.log(form.value)
+    console.log(this.myform.value)
   }
 }
