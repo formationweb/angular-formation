ab78c399c427487a02a57af001fc1a83c931555b
RSamaium <sam@newbot.io>
2024-03-26 16:38:43 +0100

    confirm directive

--- src/app/features/users/user-card/user-card.component.ts
+++ src/app/features/users/user-card/user-card.component.ts
@@ -1,20 +1,29 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { User } from '../../../core/interfaces/user.interface';
+import { ConfirmDirective } from '../../../shared/directives/confirm.directive';
 import { LangPipe } from '../../../shared/pipes/lang.pipe';
 
 @Component({
   selector: 'app-user-card',
   template: `
     <article>
+      <ng-content select="[entete]" />
       <header>{{ user.name }}</header>
       {{ user.email }}
+      <ng-content select="[footer1]" />
       <footer>
-        <button (click)="removeUser()">{{ 'REMOVE' | lang:'en' }}</button>
+        <button 
+          (click)="removeUser()" 
+          confirm="Etes vous sûr ..."
+          [confirmUsername]="user.name"
+          >
+        {{ 'REMOVE' | lang:'en' }}</button>
       </footer>
+      <ng-content select="[footer2]" />
     </article>
   `,
   standalone: true,
-  imports: [LangPipe]
+  imports: [LangPipe, ConfirmDirective]
 })
 export class UserCardComponent {
   @Input() user: User = {} as User;
