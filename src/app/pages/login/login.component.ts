d0b740002b682d9cb07c24b64cfe80f3573dff07
RSamaium <sam@newbot.io>
2024-03-27 11:23:55 +0100

    login component

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,37 +1,13 @@
-import { NgClass, NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule, NgClass, NgStyle],
-  templateUrl: './login.component.html',
-  styles: `
-    .red {
-      color: red;
-    }
-
-    .green {
-      color: green;
-    }
-
-    .bold {
-      font-weight: bold;
-    }
-  `
+  imports: [],
+  templateUrl: './login.component.html'
 })
-export class LoginComponent /*implements AfterViewInit*/ {
-  /*@ViewChild('myForm') form!: NgForm
-
-  ngAfterViewInit(): void {
-    console.log(this.form)
-  }*/
-
-  css = 'red'
-
-  login(form: NgForm) {
-    if (form.invalid) return
-    console.log(form.value)
+export class LoginComponent {
+  login() {
+    
   }
 }
