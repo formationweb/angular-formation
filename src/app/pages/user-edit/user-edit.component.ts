f55bd675377db44d0ed8729f6b15cfde0b40c50b
RSamaium <sam@newbot.io>
2024-03-27 15:48:32 +0100

    user edit - part 2

--- src/app/pages/user-edit/user-edit.component.ts
+++ src/app/pages/user-edit/user-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, inject } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { User } from '../../core/interfaces/user.interface';
 import { UserService } from '../../core/services/user.service';
@@ -6,19 +7,29 @@ import { UserService } from '../../core/services/user.service';
 @Component({
   selector: 'app-user-edit',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './user-edit.component.html',
   styleUrl: './user-edit.component.css'
 })
 export class UserEditComponent implements OnInit {
   route = inject(ActivatedRoute)
   userService = inject(UserService)
+  builder = inject(FormBuilder)
   user: User = {} as User
+
+  propEmail = new FormControl<string>('')
+  form: FormGroup = this.builder.group({
+    email: this.propEmail,
+    name: '',
+    username: ''
+  })
 
   ngOnInit(): void {
       const userId = this.route.snapshot.params['userId']
       this.userService.get(userId).subscribe((user: User) => {
         this.user = user
+        //this.propEmail.setValue(this.user.email)
+        this.form.patchValue(this.user)
       })
   }
 }
