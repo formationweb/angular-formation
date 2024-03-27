e9a59525ab1bdbb79d17c8fb0c2115ea8b566144
RSamaium <sam@newbot.io>
2024-03-27 12:12:27 +0100

    domain validator

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { domainValidator } from '../../core/validators/domain.validator';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html'
 })
 export class LoginComponent {
+  propEmail = new FormControl<string>('', [
+    Validators.required,
+    Validators.minLength(3),
+    domainValidator('hotmail.com')
+  ])
+  propPass = new FormControl<string>('')
+  myForm = new FormGroup({
+    email: this.propEmail,
+    password: this.propPass
+  })
+  submitted = false
+
   login() {
-    
+    this.submitted = true
+    if (this.myForm.invalid) return
+    console.log(this.myForm.value)
+    setTimeout(() => {
+      this.submitted = false
+    }, 1000)
   }
 }
