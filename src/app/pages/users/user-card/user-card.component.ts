f07883a1608e37080950c6f68c11b0c5e4f2161f
RSamaium <sam@newbot.io>
2024-03-27 14:08:43 +0100

    user card

--- src/app/pages/users/user-card/user-card.component.ts
+++ src/app/pages/users/user-card/user-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { User } from '../../../core/interfaces/user.interface';
 import { SharedModule } from '../../../shared/shared.module';
 
@@ -11,24 +12,26 @@ import { SharedModule } from '../../../shared/shared.module';
       {{ user.email }}
       <ng-content select="[footer1]" />
       <footer>
-        <button 
-          (eventConfirm)="removeUser()" 
+        <button
+          (eventConfirm)="removeUser()"
           confirm="Etes vous sûr ..."
           [confirmUsername]="user.name"
-          >
-        {{ 'REMOVE' | lang:'en' }}</button>
+        >
+          {{ 'REMOVE' | lang : 'en' }}
+        </button>
+        <button [routerLink]="['user', user.id]">Modifier</button>
       </footer>
       <ng-content select="[footer2]" />
     </article>
   `,
   standalone: true,
-  imports: [SharedModule]
+  imports: [SharedModule, RouterLink],
 })
 export class UserCardComponent {
   @Input() user: User = {} as User;
-  @Output() eventDelete: EventEmitter<number> = new EventEmitter()
+  @Output() eventDelete: EventEmitter<number> = new EventEmitter();
 
   removeUser() {
-    this.eventDelete.emit(this.user.id)
+    this.eventDelete.emit(this.user.id);
   }
 }
