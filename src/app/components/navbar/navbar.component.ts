3b3df0436fb61f49f5286211045ddd0cb61c7edf
RSamaium <sam@newbot.io>
2025-05-19 14:46:01 +0200

    Ajouter Pico CSS et mettre à jour le composant de recherche dans la Navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -5,11 +5,15 @@ import { SearchComponent } from "./search.component";
     selector: 'app-navbar',
     template: `
         <h1>{{ title() }}</h1>
-        <app-search [name]="userName()" />
+        <app-search [name]="userName()" (onSearch)="listenSearch($event)" />
     `,
     imports: [SearchComponent]
 })
 export class NavbarComponent {
     title = signal('Mon App')
     userName = signal('ben')
+
+    listenSearch(name: string) {
+        console.log(name)
+    }
 }
--- src/app/components/navbar/search.component.ts
+++ src/app/components/navbar/search.component.ts
@@ -1,21 +1,34 @@
-import { Component, Input, input, model, signal } from "@angular/core";
+import { Component, Input, input, model, output, signal } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 
 @Component({
     selector: 'app-search',
     template: `
         <input type="text" [(ngModel)]="name">
-        <button (click)="search()">Rechercher</button>
+        @if (name() != '') {
+            <button (click)="search()">Rechercher</button>
+        }
+        <ul>
+            @for (name of firstNames() ; track $index) {
+                <li>{{ name }}</li>
+            }
+            @empty {
+                <p>Pas de noms</p>
+            }
+        </ul>
     `,
     imports: [FormsModule]
 }) 
 export class SearchComponent {
     // model(), c'est in input(), où on peut le donner un ngModel
-    // input(), juste récupérer la valeur du composant parent
-    name = model()
+    // input(valDefault), juste récupérer la valeur du composant parent
+    // input.required()
+    name = model<string>('')
     //@Input() name = ''
+    onSearch = output<string>()
+    firstNames = signal(['ana', 'jim', 'ben'])
 
     search() {
-        console.log(this.name())
+        this.onSearch.emit(this.name())
     }
 }
