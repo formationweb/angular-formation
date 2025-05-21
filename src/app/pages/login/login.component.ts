beb7ce503b7b97c15598ee0cb2b88e012774dcfa
RSamaium <sam@newbot.io>
2025-05-21 12:01:36 +0200

    Refactor le formulaire de connexion pour utiliser Reactive Forms, ajouter la validation de domaine et améliorer la gestion des erreurs

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { domainValidator } from '../../validators/domain.validator';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  login(form: NgForm) {
-    if (form.invalid) return
+  propEmail = new FormControl('', [
+    Validators.required,
+    Validators.minLength(3),
+    domainValidator('hotmail.com')
+  ])
+  propPass = new FormControl()
+  myForm = new FormGroup({
+    email: this.propEmail,
+    password: this.propPass
+  })
+  submitted = false
+
+  login() {
+    this.submitted = true
+    if (this.myForm.invalid) return
+    console.log(this.myForm.value)
   }
 }
