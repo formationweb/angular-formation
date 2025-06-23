2f46af2dab6cfdc8e47c2c46e5724adc276218f2
RSamaium <sam@newbot.io>
2025-06-23 11:43:39 +0200

    Ajout du composant Users et mise à jour de l'application principale pour l'inclure

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core'
 import { Navbar } from './features/navbar/navbar';
+import { Users } from './features/users/users';
 
 @Component({
     selector: 'app-root',
     template: `
         <app-navbar />
+        <app-users />
     `,
-    imports: [Navbar]
+    imports: [Navbar, Users]
 })
 export class App {
 
---
+++ src/app/features/users/users.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-users',
+    template: `<h1>Utilisateurs</h1>`
+})
+export class Users {}
