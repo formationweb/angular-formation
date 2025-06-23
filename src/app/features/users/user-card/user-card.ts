9a2177f530edd043168d8590d8d282c0c3f42135
RSamaium <sam@newbot.io>
2025-06-23 15:14:20 +0200

    Ajout des composants Utilisateurs et UserCard avec l'interface User

---
+++ src/app/features/users/user-card/user-card.ts
@@ -0,0 +1,16 @@
+import { Component, input } from '@angular/core';
+import { User } from '../../../core/interfaces/user';
+
+@Component({
+  selector: 'app-user-card',
+  imports: [],
+  template: `
+    <article>
+        <header>{{ user().name }}</header>
+        <p>{{ user().email }}</p>
+    </article>
+  `
+})
+export class UserCard {
+  user = input.required<User>()
+}
