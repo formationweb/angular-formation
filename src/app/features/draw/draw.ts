25168ac29fb4c314fae573e28c596540cd8a5118
RSamaium <sam@newbot.io>
2025-06-24 13:36:51 +0200

    Mise à jour du composant Draw pour utiliser viewChildren et dessiner sur plusieurs canvases.

--- src/app/features/draw/draw.ts
+++ src/app/features/draw/draw.ts
@@ -1,22 +1,31 @@
-import { Component, effect, ElementRef, OnChanges, OnDestroy, OnInit, viewChild } from "@angular/core";
+import { Component, effect, ElementRef, OnChanges, OnDestroy, OnInit, viewChild, viewChildren } from "@angular/core";
 
 @Component({
     selector: 'app-draw',
     template: `
         <canvas #refCanvas></canvas>
+        <canvas #refCanvas></canvas>
     `
 })
 export class Draw /*implements OnInit*/ {
-    elCanvas = viewChild<ElementRef<HTMLCanvasElement>>('refCanvas')
+    //elCanvas = viewChild<ElementRef<HTMLCanvasElement>>('refCanvas')
+    elCanvas = viewChildren<ElementRef<HTMLCanvasElement>>('refCanvas')
     // @ViewChild('refCanvas') elCanvas
 
     constructor() {
         effect(() => {
-            const el = this.elCanvas()?.nativeElement
-            const context = el?.getContext('2d')
-            if (context) {
-                context.fillStyle = 'blue'
-                context.fillRect(0, 0, 100, 100)
+            // const el = this.elCanvas()?.nativeElement
+            // const context = el?.getContext('2d')
+            // if (context) {
+            //     context.fillStyle = 'blue'
+            //     context.fillRect(0, 0, 100, 100)
+            // }
+            for (let el of this.elCanvas()) {
+                const context = el?.nativeElement.getContext('2d')
+                if (context) {
+                    context.fillStyle = 'blue'
+                    context.fillRect(0, 0, 100, 100)
+                }
             }
         })
     }
