af36014d73301d6d67f549c679f0dbde17f5de5b
RSamaium <sam@newbot.io>
2025-06-25 09:42:58 +0200

    Ajout du formulaire de connexion avec validation des champs et gestion des erreurs dans le composant Login.

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, viewChild } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
+ // form = viewChild<ElementRef<HTMLFormElement>>('refForm')
 
+  login(form: NgForm) {
+    if (form.invalid) return
+    console.log(form.value)
+  }
 }
