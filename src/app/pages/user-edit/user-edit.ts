e102851446d63969a1172410e43d117c1c51bc5c
RSamaium <sam@newbot.io>
2025-06-25 15:05:13 +0200

    Ajout d'un formulaire d'édition d'utilisateur avec validation des champs et gestion des données utilisateur dans le composant UserEdit.

--- src/app/pages/user-edit/user-edit.ts
+++ src/app/pages/user-edit/user-edit.ts
@@ -3,34 +3,53 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { UserModel } from '../../core/services/user';
 import { User } from '../../core/interfaces/user';
 import { rxResource } from '@angular/core/rxjs-interop';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+interface UserEditForms {
+  name: FormControl<string | null>
+  username: string
+  email: string
+}
 
 @Component({
   selector: 'app-user-edit',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './user-edit.html',
   styleUrl: './user-edit.css'
 })
 export class UserEdit /*implements OnInit*/ {
   //private router = inject(Router)
   private userModel = inject(UserModel)
+  private builder = inject(FormBuilder)
   id = input.required({
    // transform: (value: string) => +value
    transform: numberAttribute
   })
   // user = rxResource({
   //   params: () => this.id(),
-  //   loader: ({ params }) => this.userModel.get(params)
+  //   stream: ({ params }) => this.userModel.get(params)
   // })
   user = signal<User>({} as User)
+
+  propName = new FormControl()
+  form = this.builder.group<UserEditForms>({
+    name: this.propName,
+    username: '',
+    email: ''
+  })
 
   constructor() {
     effect(() => {
        this.userModel.get(this.id()).subscribe((resUser) => {
         this.user.set(resUser)
+        // this.propName.setValue(this.user().name)
+        this.form.patchValue(this.user())
        })
     })
     //this.router.navigateByUrl('/')
   }
+
+  edit() {}
 
   // private route = inject(ActivatedRoute)
 
