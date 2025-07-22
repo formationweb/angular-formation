7254ee39efc8d048522fddac0472fe3bb50c7503
RSamaium <sam@newbot.io>
2025-07-22 11:33:32 +0200

    Ajout d'un pipe Lang pour la gestion des traductions, mise à jour du composant UserCard pour utiliser le pipe Lang et ajout d'un bouton de suppression avec texte traduit, et correction de l'affichage du nombre d'utilisateurs filtrés dans le template.

--- src/app/users/user-card.ts
+++ src/app/users/user-card.ts
@@ -1,5 +1,6 @@
 import { Component, input } from "@angular/core";
 import { User } from "../core/interfaces/user";
+import { LangPipe } from "../core/pipes/lang";
 
 @Component({
     selector: 'app-user-card',
@@ -7,8 +8,12 @@ import { User } from "../core/interfaces/user";
         <article>
             <header>{{ user().name }}</header>
             <p>{{ user().email }}</p>
+            <footer>
+                <button>{{ 'REMOVE' | lang:'fr' }}</button>
+            </footer>
         </article>
-    `
+    `,
+    imports: [LangPipe]
 })
 export class UserCard {
     user = input.required<User>()
