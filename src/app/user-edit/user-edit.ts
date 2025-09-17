65c3043afad5170960c7300b004a0842a914014a
RSamaium <sam@newbot.io>
2025-09-17 15:24:49 +0200

    Ajouter un formulaire d'édition d'utilisateur avec validation dans le composant UserEdit

--- src/app/user-edit/user-edit.ts
+++ src/app/user-edit/user-edit.ts
@@ -2,21 +2,30 @@ import { Component, effect, inject, input, numberAttribute, OnInit, signal } fro
 import { ActivatedRoute } from '@angular/router';
 import { UsersService } from '../users/users.service';
 import { UserModel } from '../models/user';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-edit',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './user-edit.html',
   styleUrl: './user-edit.css'
 })
 export class UserEdit {
   private usersService = inject(UsersService)
+  private builder = inject(FormBuilder)
   user = signal<UserModel>({} as UserModel)
   // private route = inject(ActivatedRoute)
 
   // ngOnInit(): void {
   //   this.route.snapshot.params['id']
   // }
+
+  propName = new FormControl('')
+  form = this.builder.group({
+    name: this.propName,
+    username: '',
+    email: ''
+  })
 
   id = input.required({
     transform: numberAttribute
@@ -27,6 +36,7 @@ export class UserEdit {
       this.usersService.get(this.id()).subscribe({
         next: (user) => {
           this.user.set(user)
+          this.form.patchValue(user)
         },
         error: (err) => {
           console.log(err)
@@ -34,4 +44,6 @@ export class UserEdit {
       })
     })
   }
+
+  editUser() {}
 }
