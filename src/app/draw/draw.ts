6d2a89f4bd0ccd1b1893086ff2a6a1aaa3268e40
RSamaium <sam@newbot.io>
2025-11-04 14:08:38 +0100

    draw

--- src/app/draw/draw.ts
+++ src/app/draw/draw.ts
@@ -1,23 +1,27 @@
-import { AfterViewInit, Component, effect, ElementRef, OnInit, ViewChild, viewChild } from '@angular/core';
+import { AfterViewInit, Component, effect, ElementRef, OnInit, ViewChild, viewChild, viewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-draw',
   imports: [],
   template: `
     <canvas #canvasRef></canvas>
+    <canvas #canvasRef></canvas>
   `
 })
 export class Draw  {
-   canvasEl = viewChild<ElementRef<HTMLCanvasElement>>('canvasRef')
+   //canvasEl = viewChild<ElementRef<HTMLCanvasElement>>('canvasRef')
+   canvasArray = viewChildren<ElementRef<HTMLCanvasElement>>('canvasRef')
 
    constructor() {
     effect(() => {
-      const canvas = this.canvasEl()?.nativeElement
-      if (canvas) {
-        const context = canvas.getContext('2d')
-        if (context) {
-          context.fillStyle = 'blue'
-          context.fillRect(0, 0, 100, 100)
+      for (let canvasEl of this.canvasArray()) {
+        const canvas = canvasEl?.nativeElement
+        if (canvas) {
+          const context = canvas.getContext('2d')
+          if (context) {
+            context.fillStyle = 'blue'
+            context.fillRect(0, 0, 100, 100)
+          }
         }
       }
     })
