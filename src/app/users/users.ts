73238da38cb393e911180bbdfda193097d853aa8
RSamaium <sam@newbot.io>
2025-11-04 14:58:06 +0100

    users viewchild

--- src/app/users/users.ts
+++ src/app/users/users.ts
@@ -1,4 +1,4 @@
-import { Component, computed, signal } from "@angular/core";
+import { Component, computed, ElementRef, signal, viewChildren } from "@angular/core";
 import { UserCard } from "./user-card";
 import { User } from "../core/interfaces/user";
 import { Loader } from "../atomics/loader";
@@ -13,6 +13,7 @@ import { Draw } from "../draw/draw";
     imports: [UserCard, Loader, Opacity, FormsModule, PluralPipe, Draw],
 })
 export class Users {
+    
     extensions = signal(['tv', 'biz', 'io', 'me'])
     extSelected = signal('')
   
@@ -256,7 +257,16 @@ export class Users {
         .filter(user => user.email.endsWith(this.extSelected()))
     })
 
+    cardEl = viewChildren<ElementRef<HTMLDivElement>>('userRef')
+    cardDiv = computed<ElementRef<HTMLDivElement> | undefined>(() => this.cardEl()[this.userIndex()])
+    userIndex = signal(0)
+    errorMessage = computed(() => this.cardDiv() ? '' : 'Index invalide')
+
     listenOpacity(opacity: number) {
       console.log(opacity)
+    }
+
+    scrollToUser() {
+       this.cardDiv()?.nativeElement.scrollIntoView({ behavior: 'smooth', block: 'center' });
     }
 }
