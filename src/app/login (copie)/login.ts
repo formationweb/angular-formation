d370ba290bcd14f1befd70f276ea1b3e1339dc19
RSamaium <sam@newbot.io>
2025-11-05 11:47:57 +0100

    login reactive forms

---
+++ src/app/login (copie)/login.ts
@@ -0,0 +1,17 @@
+import { Component, viewChild } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  imports: [FormsModule],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+})
+export class Login {
+  login(form: NgForm) {
+    if (form.invalid) {
+      return
+    }
+    console.log(form.value)
+  }
+}
