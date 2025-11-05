74e4b519e5b3a34c3cdabac4109cbe9eaaa5e4e9
RSamaium <sam@newbot.io>
2025-11-05 16:44:58 +0100

    confirm directive

--- src/app/users/user-card.ts
+++ src/app/users/user-card.ts
@@ -15,7 +15,7 @@ import { ConfirmDirective } from "../core/directives/confirm";
             <ng-content select=".footer" />
             <footer>
                 <button 
-                (click)="onDelete.emit(user().id)" 
+                (onConfirm)="onDelete.emit(user().id)" 
                 confirm="Etes vous sûr ..."
                 [confirmUsername]="user().name"
                 >
