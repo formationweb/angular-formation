0c6b0d845b2921b6940e08fb1b2c06c4b9bc4da8
RSamaium <sam@newbot.io>
2026-03-16 16:16:34 +0100

    user input

---
+++ src/app/users/user.interface.ts
@@ -0,0 +1,11 @@
+export interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  // ---pas utilisé --
+  address: any;
+  phone: string;
+  website: string;
+  company: any;
+}
