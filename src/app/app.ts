55939f8f9662a4b6a368a072a2b064c68c7d5332
RSamaium <sam@newbot.io>
2026-03-16 15:04:02 +0100

    navbar input/output

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core'
+import { Component, signal } from '@angular/core'
 import { Navbar } from './navbar/navbar';
 import { Users } from './users/users';
 
 @Component({
     selector: 'app-root',
     template: `
-        <app-navbar />
+        <app-navbar 
+            [title]="myTitle()" 
+            userName="ana" 
+            (onSearch)="listenSearch($event)" 
+        />
         <app-users />
     `,
     imports: [Navbar, Users]
 })
-export class App {}
+export class App {
+    myTitle = signal('test')
+
+    listenSearch(userName: string) {
+        console.log(userName)
+    }
+}
--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -1,7 +1,38 @@
-import { Component } from "@angular/core";
+import { Component, Input, input, model, output, signal } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
 @Component({
     selector: 'app-navbar',
-    template: `<h1>Navbar</h1>`
+    template: `
+        <h1>{{ title() }}</h1>
+        <input type="text" [(ngModel)]="userName">
+        @if (userName() != '') {
+            <button (click)="search()">Rechercher</button>
+        }
+        <ul>
+            @for (name of names() ; track $index) {
+                <li [class]="$odd ? 'red' : ''">{{ $odd }} - {{ name }}</li>
+            }
+            @empty {
+                <p>Vide</p>
+            }
+        </ul>
+    `,
+    imports: [FormsModule],
+    styles: `
+        .red {
+            color: red;
+        }
+    `
 })
-export class Navbar {}
+export class Navbar {
+    title = input.required() // avoir un attribut html personnalisé en lecture seule
+    userName = model('') // avoir un attribut html personnalisé en lecture/écriture
+    onSearch = output<string>() // avoir un attribut html personnalisé pour notre propre événement
+
+    names = signal(['ana', 'ben', 'jim', 'ana'])
+
+    search() {
+       this.onSearch.emit(this.userName())
+    }
+}
