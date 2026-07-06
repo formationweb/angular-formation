89503e095e0fd9a12947434ebfda497855fd693a
RSamaium <sam@newbot.io>
2026-07-06 11:31:34 +0200

    Add Users component and integrate it into the App component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component } from "@angular/core";
 import { Navbar } from "./navbar/navbar";
+import { Users } from "./users/users";
 
 @Component({
     selector: 'app-root',
     template: `
-        <h1>Hello World</h1>
         <app-navbar />
+        <app-users />
     `,
-    imports: [Navbar]
+    imports: [Navbar, Users]
 })
 export class App {}
---
+++ src/app/users/users.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-users',
+    template: `
+        <h1>Users</h1>
+    `
+})
+export class Users {}
